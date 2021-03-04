53b9ad4560a9b68997b4cdcee82cd352fb5940de
Serhii V <serhii.voronovych@gmail.com>
2021-03-04 18:01:25 +0800

    - get articles list

--- app/Containers/Article/UI/API/Routes/GetAllArticles.v1.public.php
+++ app/Containers/Article/UI/API/Routes/GetAllArticles.v1.public.php
@@ -1,29 +1,29 @@
 <?php
 
-///**
-// * @apiGroup           Article
-// * @apiName            getAllArticles
-// *
-// * @api                {GET} /v1/articles Endpoint title here..
-// * @apiDescription     Endpoint description here..
-// *
-// * @apiVersion         1.0.0
-// * @apiPermission      none
-// *
+/**
+ * @apiGroup           Article
+ * @apiName            getAllArticles
+ *
+ * @api                {GET} /v1/articles Get articles list
+ * @apiDescription     Get articles. All or Filtered.
+ *
+ * @apiVersion         1.0.0
+ * @apiPermission      none
+ *
 // * @apiParam           {String}  parameters here..
-// *
-// * @apiSuccessExample  {json}  Success-Response:
-// * HTTP/1.1 200 OK
-//{
-//  // Insert the response of the request here...
-//}
-// */
-//
-///** @var Route $router */
-//$router->get('articles', [
-//    'as' => 'api_article_get_all_articles',
-//    'uses'  => 'Controller@getAllArticles',
+ *
+ * @apiSuccessExample  {json}  Success-Response:
+ * HTTP/1.1 200 OK
+{
+  // Insert the response of the request here...
+}
+ */
+
+/** @var Route $router */
+$router->get('articles', [
+    'as' => 'api_article_get_all_articles',
+    'uses'  => 'Controller@getAllArticles',
 //    'middleware' => [
 //      'auth:api',
 //    ],
-//]);
+]);
