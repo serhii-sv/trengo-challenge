f4af504f8cacf44827e48e5fb960afc6887d277a
Serhii V <serhii.voronovych@gmail.com>
2021-03-04 18:20:19 +0800

    - daily limits

--- app/Containers/Vote/Actions/CreateVoteAction.php
+++ app/Containers/Vote/Actions/CreateVoteAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Containers\Vote\Actions;
 
+use App\Containers\Vote\Models\Vote;
 use App\Ship\Parents\Actions\Action;
 use App\Ship\Parents\Requests\Request;
 use Apiato\Core\Foundation\Facades\Apiato;
@@ -17,6 +18,31 @@ class CreateVoteAction extends Action
 
         $data['ip_address'] = $request->ip();
 
+        /*
+         * Check daily limits
+         */
+        $checkDailyLimits = Vote::where('ip_address', $data['ip_address'])
+          ->where('created_at', '>=', now()->subDay())
+          ->count();
+
+        if ($checkDailyLimits > env('VOTE_DAILY_LIMIT', 10)) {
+          throw new \Exception('Vote daily limit exceed.');
+        }
+
+        /*
+         * Check exists votes for this IP
+         */
+        $checkExistsVotes = Vote::where('ip_address', $data['ip_address'])
+          ->where('article_id', $data['article_id'])
+          ->count();
+
+        if ($checkExistsVotes > 0) {
+          throw new \Exception('This Articles already voted');
+        }
+
+        /*
+         * Register Vote
+         */
         $vote = Apiato::call('Vote@CreateVoteTask', [$data]);
 
         return $vote;
